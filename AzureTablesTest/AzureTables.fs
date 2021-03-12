02d34a3536a8df26c8afb1e9649b3437e56db4ff
Patrick Johnstone <patrick.johnstone@spektrix.com>
2021-03-12 10:38:50 +0000

    Break Table functions out to module

---
+++ AzureTablesTest/AzureTables.fs
@@ -0,0 +1,12 @@
+﻿module AzureTables
+
+open Microsoft.Azure.Cosmos.Table
+
+let insertOperation (person: ITableEntity) =
+  TableOperation.Insert(person)
+
+let executeOperation (table: CloudTable) operation: Result<TableResult, StorageException> =
+  try
+    Ok(table.Execute(operation))
+  with
+    | :? StorageException as ex -> Error(ex)
--- AzureTablesTest/Person.fs
+++ AzureTablesTest/Person.fs
@@ -1,36 +1,28 @@
 ﻿module Person
 
 open ResultBuilder
+open AzureTables
 open Microsoft.Azure.Cosmos.Table
 
 type T = {FirstName:string; LastName:string; Email:string; Phone:string}
 
-type PersonDto(firstName, lastName, email: string, phone: string) =
+type Dto(firstName, lastName, email: string, phone: string) =
   inherit TableEntity(partitionKey=lastName, rowKey=firstName)
-  new() = PersonDto(null, null, null, null)
+  new() = Dto(null, null, null, null)
   member val Email = email with get, set
   member val PhoneNumber = phone with get, set
 
 let fromDomain person =
-  PersonDto(person.FirstName, person.LastName, person.Email, person.Phone)
+  Dto(person.FirstName, person.LastName, person.Email, person.Phone)
 
-let toDomain (dto: PersonDto) =
+let toDomain (dto: Dto) =
   result {
     return {FirstName=dto.RowKey; LastName=dto.PartitionKey; Email=dto.Email; Phone=dto.PhoneNumber}
-  }     
-   
-let insertOperation (person: PersonDto) =
-  TableOperation.Insert(person)
-
-let executeOperation (table: CloudTable) operation: Result<TableResult, StorageException> =
-  try
-    Ok(table.Execute(operation))
-  with
-    | :? StorageException as ex -> Error(ex)
+  }
 
 let save person (table: CloudTable): Result<TableResult, StorageException> =
-    let executor = executeOperation table
-    person
-    |> fromDomain
-    |> insertOperation
-    |> executor
+  let executor = executeOperation table
+  person
+  |> fromDomain
+  |> insertOperation
+  |> executor
